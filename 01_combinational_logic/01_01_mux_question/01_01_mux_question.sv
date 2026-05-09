1c8022cfb753f804076ee6a4d5347d1271c49c37
Chris Khoo <chris.khoo@gmail.com>
2026-05-08 20:54:36 -0700

    being ocd with 01_01

--- 01_combinational_logic/01_01_mux_question/01_01_mux_question.sv
+++ 01_combinational_logic/01_01_mux_question/01_01_mux_question.sv
@@ -24,9 +24,6 @@ module mux_4_1
   output [3:0] y
 );
 
-  // Task:
-  // Using code for mux_2_1 as an example,
-  // write code for 4:1 mux using "?:" operator
-
+  assign y = !sel[1] ? (!sel[0] ? d0 : d1) : (!sel[0] ? d2 : d3);
 
 endmodule
